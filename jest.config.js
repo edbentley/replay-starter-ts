efa75c830053d36180681cd255f5b373a2489de4
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2020-05-02 15:38:23 +0100

    v0.1.0

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  transformIgnorePatterns: ["/node_modules/(?!@replay/).+\\.js$"],
+};
